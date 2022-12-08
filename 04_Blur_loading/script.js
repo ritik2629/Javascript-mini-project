a42960020abbdbdae321a0659254a8b14daf85bd
Ritik Raj <ritikraj2629@gmail.com>
2022-12-08 12:40:17 +0530

    Fourth Blur Loading

---
+++ 04_Blur_loading/script.js
@@ -0,0 +1,23 @@
+const bg =document.querySelector('.bg')
+const ltext =document.querySelector('.loading-text')
+
+let load =0;
+
+let int =setInterval(()=>{
+    blurring()
+},30)
+
+const blurring=()=>{
+    load++;
+    if(load > 99){
+        clearInterval(int)
+    }
+    console.log(load)
+    ltext.innerText=`${load}%`
+    ltext.style.opacity = scale(load, 0, 100, 1, 0)
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+  }
